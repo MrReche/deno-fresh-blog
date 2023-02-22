f4bb1f7805824dc11b15bb73611b41bbfe9d4939
Mr.Reche <pqu@lovecoding.it>
2023-02-22 11:52:11 +0100

    First push

---
+++ content/posts/second-post.md
@@ -0,0 +1,11 @@
+---
+title: Second Post
+date: 2022-02-24
+excerpt: This is my second post
+---
+
+Edén Alba Carapan
+
+Es un tipo muy corriente
+
+Tambien puede bailar y deletrear de forma mas o menos curiosa.
---
+++ utils/posts_tests.ts
@@ -0,0 +1,13 @@
+import { loadPost } from "./posts.ts";
+import { assertEquals } from "$std/testing/asserts.ts";
+
+Deno.test("loadPost() return null is doesn exists", async () => {
+  const post = await loadPost("non-existent");
+  assertEquals(post, null);
+});
+
+Deno.test("loadPost() return a pot object if post exists", async () => {
+  const post = await loadPost("hello-world");
+  assertEquals(post?.id, "hello-world");
+  assertEquals(post?.title, "Hello World");
+});
